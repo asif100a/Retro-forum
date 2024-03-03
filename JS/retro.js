6d2e47d92bc08d6de74ab25491aba0ed6e7028ea
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-03-03 23:01:11 +0600

    Start to view read data

--- JS/retro.js
+++ JS/retro.js
@@ -9,7 +9,8 @@ const showProductionOfCard1 = async () => {
     const dataPosts = data.posts;
     // console.log(dataPosts);
 
-    displayData(dataPosts)
+    displayData(dataPosts);
+    showViewData(dataPosts);
 };
 
 const displayData = (dataAll) => {
@@ -19,7 +20,7 @@ const displayData = (dataAll) => {
     dataAll.forEach(element => {
         console.log(element);
         const divForCard1 = document.createElement('div');
-    divForCard1.innerHTML = `
+        divForCard1.innerHTML = `
     <div class="border-2 bg-[#F3F3F5] w-auto lg:w-[48rem] flex flex-col lg:flex-row gap-6 py-10 px-8 rounded-3xl">
     <div class="relative h-fit w-fit p-4">
         <img src="${element.image}" alt="" class="w-24 h-20 rounded-xl">
@@ -32,7 +33,7 @@ const displayData = (dataAll) => {
         <p class="mt-4 text-base font-normal text-[#12132D99]">
             ${element.description}</p>
 
-        <hr class="my-5">
+        <hr class="my-5 w-auto lg:w-[35rem]">
 
         <div class="flex justify-between">
             <div class="flex flex-col lg:flex-row gap-6">
@@ -50,15 +51,34 @@ const displayData = (dataAll) => {
                 </ul>
             </div>
             <div>
-                <img src="images/Message.svg" alt="Message" class="">
+                <img onclick="showViewData()" src="images/Message.svg" alt="Message" class="hover:cursor-pointer">
             </div>
         </div>
     </div>
     </div>
     `;
 
-    cardsContainer.appendChild(divForCard1);
+        cardsContainer.appendChild(divForCard1);
+
     });
 
+};
+
+const showViewData = (element) => {
+    console.log(element)
+    const showView = document.getElementById('Views-content');
+    // console.log(showView);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="bg-white w-fit flex items-center p-4 rounded-2xl mt-8">
+        <p class="font-semibold w-auto lg:w-64">${element}</p>
+        <p class="flex flex-col lg:flex-row gap-1 lg:gap-3">
+            <span><img src="images/Views.svg" alt="Views"></span>
+            <span>1,568</span>
+        </p>
+    </div>
+    `;
+    showView.appendChild(div);
 };
 showProductionOfCard1();

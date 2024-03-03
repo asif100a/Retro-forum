cbb29283a157ad0aa85b39ff4955fe88337bf682
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-03-03 22:09:18 +0600

    taking all data from api is finished

--- JS/retro.js
+++ JS/retro.js
@@ -4,27 +4,33 @@
 
 // 
 const showProductionOfCard1 = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts?category=coding');
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     const dataPosts = data.posts;
-    console.log(dataPosts[0].image)
+    // console.log(dataPosts);
 
+    displayData(dataPosts)
+};
+
+const displayData = (dataAll) => {
+    console.log(dataAll);
     const cardsContainer = document.getElementById('cards-container');
 
-    const divForCard1 = document.createElement('div');
+    dataAll.forEach(element => {
+        console.log(element);
+        const divForCard1 = document.createElement('div');
     divForCard1.innerHTML = `
     <div class="border-2 bg-[#F3F3F5] w-auto lg:w-[48rem] flex flex-col lg:flex-row gap-6 py-10 px-8 rounded-3xl">
     <div class="relative h-fit w-fit p-4">
-        <img src="${dataPosts[0].image}" alt="" class="w-24 h-20 rounded-xl">
+        <img src="${element.image}" alt="" class="w-24 h-20 rounded-xl">
         <p class="w-3 h-3 bg-red-500 rounded-full absolute top-3 right-3"></p>
     </div>
 
     <div>
-        <p class="mb-4 text-[#12132DCC] text-sm"><span># ${dataPosts[0].category}</span> <span>Author : ${dataPosts[0].author.name}</span></p>
-        <h5 class="text-xl font-bold">10 Kids Unaware of Their Halloween Costume</h5>
+        <p class="mb-4 text-[#12132DCC] text-sm"><span class="mr-3"># ${element.category}</span> <span>Author : ${element.author.name}</span></p>
+        <h5 class="text-xl font-bold">${element.title}</h5>
         <p class="mt-4 text-base font-normal text-[#12132D99]">
-            It's one thing to subject yourself to ha Halloween costume mishap because,
-            hey that's your prerogative</p>
+            ${element.description}</p>
 
         <hr class="my-5">
 
@@ -32,15 +38,15 @@ const showProductionOfCard1 = async () => {
             <div class="flex flex-col lg:flex-row gap-6">
                 <ul class="flex space-x-3">
                     <li><img src="images/Comments.svg" alt="Comments"></li>
-                    <li>560</li>
+                    <li>${element.comment_count}</li>
                 </ul>
                 <ul class="flex space-x-3">
                     <li><img src="images/Views.svg" alt="Comments"></li>
-                    <li>1,568</li>
+                    <li>${element.view_count}</li>
                 </ul>
                 <ul class="flex space-x-3">
                     <li><img src="images/Duration.svg" alt="Comments"></li>
-                    <li>5 mins</li>
+                    <li>${element.posted_time} mins</li>
                 </ul>
             </div>
             <div>
@@ -52,5 +58,7 @@ const showProductionOfCard1 = async () => {
     `;
 
     cardsContainer.appendChild(divForCard1);
+    });
+
 };
 showProductionOfCard1();
